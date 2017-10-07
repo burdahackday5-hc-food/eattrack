88d0d3a3d013af63fc946ec5d6b5a6267e3d1c66
plusgut <github@webentwickler2-0.de>
2017-10-07 17:40:03 +0200

    [filters] removed some static code

--- src/Filter/index.tsx
+++ src/Filter/index.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import './filter.css';
+import answers from '../constants/answers';
 
 interface props {
   value: number;
   onChange: (value: number) => void;
 }
 
-const FILTERS = ['All', 'Kohlenhydrate', 'Small Portion', 'Low Sodium'];
+const filters = ['All'].concat(answers);
 
 class Filter extends React.Component<props, {}> {
   public render() {
     return (
       <ul>
-        {FILTERS.map((filter, index) =>
+        {filters.map((filter, index) =>
           <li key={index}
             onClick={() => this.props.onChange(index)}
             className={this.props.value === index ? 'filter--activated' : ''}>{filter}</li>,
